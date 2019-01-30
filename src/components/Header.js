5901a573ca0ce7e3212fd76704fd6f3462338de1
Antti Kalliokoski <antti.a.kalliokoski@gmail.com>
2019-01-30 09:31:32 +0200

    Update title

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ export default function Header() {
         <span role="img" aria-label="fire">
           🔥
         </span>{" "}
-        FHIRcast WebSocket Demo
+        FHIRcast WebSocket Client
       </div>
       <ul className="navbar-nav">
         <li className="nav-item mx-2">
