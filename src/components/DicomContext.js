600470f0a7ba615093e9f7c2820eefe14562b4e8
Antti Kalliokoski <antti.a.kalliokoski@gmail.com>
2019-03-06 10:35:07 +0200

    Show DICOM image only when context exists

--- src/components/DicomContext.js
+++ src/components/DicomContext.js
@@ -3,21 +3,24 @@ import PropTypes from "prop-types";
 import DicomViewer from "./DicomViewer";
 
 const getDcmUrls = (context) => {
-  if (!Array.isArray(context) || context.length === 0) {
+  if (!isContext(context)) {
     return [];
   }
 
   return ["https://raw.githubusercontent.com/ivmartel/dwv/master/tests/data/bbmri-53323851.dcm"];
 }
 
+const isContext = ctx => Array.isArray(ctx) && ctx.length > 0;
+
 function DicomContext({ context } = {}) {
   const urls = getDcmUrls(context);
+  const hasUrls = urls.length > 0;
   return (
     <div className="fc-card">
       <div className="card">
         <h5 className="card-header">DICOM Context</h5>
         <div className="card-body">
-          <DicomViewer urls={urls} />
+          {hasUrls ? <DicomViewer urls={urls} /> : null} 
         </div>
       </div>
     </div>
